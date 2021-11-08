b3c286f115f9fe2f21ddae57f3628d92009e5007
abennett92 <saiford92@gmail.com>
2021-11-07 21:59:34 -0500

    finished

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express")
+const app = express()
+const port = 8000
+var faker = require('faker')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+const users = [];
+const companies = [];
+
+class User {
+    constructor() {
+        this._id = faker.datatype.uuid()
+        this.firstName = faker.name.firstName()
+        this.lastName = faker.name.lastName()
+        this.phoneNumber = faker.phone.phoneNumber()
+        this.email = faker.internet.email()
+        this.pasword = faker.internet.password()
+    }
+}
+
+class Company {
+    constructor() {
+        this._id = faker.datatype.uuid()
+        this.name = faker.company.companyName()
+        this.address = {
+            street: faker.address.streetName(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCodeByState(),
+            country: faker.address.country()
+        }
+    }
+}
+
+
+app.get("/api/users", (req, res) => {
+    res.json(users)
+}
+)
+
+app.get("/api/users/new", (req, res) => {
+    let newUser = new User()
+    users.push(newUser)
+    res.json({data: newUser})
+}
+)
+
+app.get("/api/companies", (req, res) => {
+    res.json(companies)
+}
+)
+
+app.get("/api/companies/new", (req, res) => {
+    let newComp = new Company()
+    companies.push(newComp)
+    res.json({data: newComp})
+}
+)
+
+app.get("/api/user/company", (req, res) => {
+    let newUser = new User()
+    users.push(newUser)
+    let newComp = new Company()
+    companies.push(newComp)
+    res.json({user: newUser, company: newComp})
+}
+)
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
